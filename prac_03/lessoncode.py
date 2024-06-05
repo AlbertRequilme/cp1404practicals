6a857c1858d6dc4618b658344e51a99cf8c2e2b0
AlbertRequilme <albertjamesredido.requilme@my.jcu.edu.au>
2024-06-05 18:20:21 +0800

    added some notes

--- prac_03/lessoncode.py
+++ prac_03/lessoncode.py
@@ -10,4 +10,67 @@ def do_this_now_1():
 # Use For loops to read many lines.
 
 
-do_this_now_1()
+def do_this_now_2():
+    s = "\tPython, Monty\n"
+    print(s)
+    print(s[1], ".", sep="")
+    print(s.strip(), ".", sep="")
+    s = s.replace(' ', '*')
+    print(s)
+    print(s.lstrip(),".", sep="")
+    print(s.strip().split(','))
+
+
+def do_this_now_3():
+    name = input("Name: ")
+    with open('name.txt', 'w') as out_file:
+        print(name, file=out_file)
+# This is the start of using "with x" as with allows you to open files without having to type .close()
+
+def validate_input():
+    age = 0
+    valid_input = False
+    while not valid_input:
+        try:
+            age = int(input("Age: "))
+            valid_input = True
+        except ValueError:
+            print("Invalid input (Not an integer)")
+        else:
+            print(f"Next year you will be {age + 1}.")
+        finally:
+            print("Good Try.")
+
+
+def do_this_now_4():
+    is_finished = False
+    result = 0
+    while not is_finished:
+        try:
+            result = int(input("Enter a valid integer: "))
+            is_finished = True
+        except ValueError:
+            print("An error occurred (Invalid Integer).")
+    print("Valid result is:", result)
+
+
+def do_this_now_5():
+    try:
+        value = int(input("> "))
+        result = f"{value}" * value
+        print(result)
+    except TypeError:
+        print("Invalid Calculation")
+
+
+# whats_wrong_with_the_demo_in_video_1():
+
+
+
+
+# do_this_now_1()
+# do_this_now_2()
+# do_this_now_3()
+# do_this_now_4()
+# do_this_now_5()
+# validate_input()
