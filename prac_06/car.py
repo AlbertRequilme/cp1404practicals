39b1e5eb0af8e19debb1210bc40636fb48108a70
AlbertRequilme <albertjamesredido.requilme@my.jcu.edu.au>
2024-07-14 21:27:43 +0800

    added doc strings

--- prac_06/car.py
+++ prac_06/car.py
@@ -32,4 +32,5 @@ class Car:
         return distance
 
     def __str__(self):
+        """Returns the object as a proper string"""
         return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,14 +1,17 @@
 class ProgrammingLanguage:
-    """A way to choose your programming language"""
+    """Represents a Programming Language"""
 
     def __init__(self, name="", dynamic="", reflection=False, year=0):
+        """initialise the name, dynamic, reflection, and year of ProgrammingLanguage"""
         self.name = name
         self.dynamic = dynamic
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Returns True if Dynamic, else False"""
         return self.dynamic == "Dynamic"
 
     def __str__(self):
+        """Returns the object as a proper string"""
         return f"{self.name}, {self.dynamic} Typing, Reflection={self.reflection}, First appeared in {self.year}"
