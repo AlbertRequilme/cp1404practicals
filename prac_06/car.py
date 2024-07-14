1b72d72c716bd936c52e935430e7e6ad6cdc7e7f
AlbertRequilme <albertjamesredido.requilme@my.jcu.edu.au>
2024-07-14 20:52:19 +0800

    completed cars.py and used_cars.py

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, fuel=0, name=""):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self._odometer = 0
+        self.name = name
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -29,3 +30,6 @@ class Car:
             self.fuel -= distance
         self._odometer += distance
         return distance
+
+    def __str__(self):
+        return f"{self.name}, fuel={self.fuel}, odometer={self._odometer}"
--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,10 +9,16 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car(180, "Car")
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+    # This is where the Limo Section starts
+    my_limo = Car(100, "Limo")
+    my_limo.add_fuel(20)
+    print(f"Limo has fuel: {my_limo.fuel}")
+    my_limo.drive(115)
+    print(my_limo)
 
 
 main()
